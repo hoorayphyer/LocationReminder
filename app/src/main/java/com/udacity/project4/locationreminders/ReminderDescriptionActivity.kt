9b70d055a73a9a7fe2111f5f772f7992f5cd875b
hooray <hrfudan@gmail.com>
2021-07-10 12:07:23 -0400

    filled in ReminderDescriptionActivity

--- app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -32,6 +32,12 @@ class ReminderDescriptionActivity : AppCompatActivity() {
             this,
             R.layout.activity_reminder_description
         )
-//        TODO: Add the implementation of the reminder details
+
+        val reminder = intent.extras!![EXTRA_ReminderDataItem] as ReminderDataItem
+        binding.detailTitle.text = reminder.title
+        binding.detailLocation.text = reminder.location
+        reminder.description?.let{
+            binding.detailDescription.text =it
+        }
     }
 }
