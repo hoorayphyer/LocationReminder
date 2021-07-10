baf034677e100753f453477debb70723837a5647
hooray <hrfudan@gmail.com>
2021-07-09 22:20:08 -0400

    wrote and passed two fragment tests

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,19 +1,16 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
-import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -24,14 +21,30 @@ import org.mockito.Mockito.verify
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest {
-
-    //    TODO: test the navigation of the fragments.
-    //    TODO: test the displayed data on the UI.
     //    TODO: add testing for the error messages.
     @Test
-    fun activeReminderList_DisplayedInUi() {
+    fun activeReminderList_NoDataBoxDisplayedInUi() {
         launchFragmentInContainer<ReminderListFragment>(null, R.style.AppTheme)
-        Thread.sleep(2000)
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigateToAddReminderScreen() {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(null, R.style.AppTheme)
+        val navController = mock(NavController::class.java) // a mock navController
+
+        // make scenario use the mock navController
+        scenario.onFragment {
+            Navigation.setViewNavController(it.view!!, navController)
+        }
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // verify
+        verify(navController).navigate(
+            // this statement is from safeargs
+            ReminderListFragmentDirections.toSaveReminder()
+        )
     }
 
 }
