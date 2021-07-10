9222c34e84633dd9d58c9af201d960462e31c65f
hooray <hrfudan@gmail.com>
2021-07-10 11:40:03 -0400

    wrote tests for ReminderLocalRepository

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
@@ -25,6 +26,67 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+
+    private lateinit var localDataSource: ReminderDataSource
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun setup() {
+        // Using an in-memory database for testing, because it doesn't survive killing the process.
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        localDataSource =
+            RemindersLocalRepository(
+                database.reminderDao(),
+                Dispatchers.Main
+            )
+    }
+
+    @After
+    fun cleanUp() {
+        database.close()
+    }
+
+
+    // runBlocking is used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // Replace with runBlockingTest once issue is resolved
+    @Test
+    fun saveReminder_retrievesReminder() = runBlocking {
+        // GIVEN - A new reminder saved in the database.
+        val reminder = ReminderDTO("title", "description", "location", 0.0, 0.0, "myId")
+        localDataSource.saveReminder(reminder)
+
+        // WHEN  - reminder retrieved by ID.
+        val result = localDataSource.getReminder("myId")
+
+        // THEN - Same reminder is returned.
+        assertThat(result, instanceOf(Result.Success::class.java))
+        result as Result.Success
+        assertThat(result.data.id, `is`("myId"))
+        assertThat(result.data.title, `is`("title"))
+        assertThat(result.data.description, `is`("description"))
+        assertThat(result.data.location, `is`(reminder.location))
+        assertThat(result.data.latitude, `is`(reminder.latitude))
+        assertThat(result.data.longitude, `is`(reminder.longitude))
+    }
+
+    // runBlocking is used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // Replace with runBlockingTest once issue is resolved
+    @Test
+    fun retrievesNonExistentReminder() = runBlocking {
+        // WHEN  - a non-exsitent ID was used.
+        val result = localDataSource.getReminder("non-existent-id")
+
+        // THEN - Same task is returned.
+        assertThat(result, instanceOf(Result.Error::class.java))
+    }
 }
